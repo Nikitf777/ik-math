ecc4fb5b9d15b6552ac7a7f2501fa6fbd6f9e51f
Nikita Samusev <nikitf777@gmail.com>
2025-09-28 14:39:00 +0300

    Add support for custom clamp range

--- src/two_bone.rs
+++ src/two_bone.rs
@@ -8,23 +8,41 @@ pub fn calculate_point_between_stretched(
 	first_len: f64,
 	second_len: f64,
 	stretch_factor: f64,
-) -> Vector2 {
+	min_target_distance_ratio: f64,
+	max_target_distance_ratio: f64,
+) -> (Vector2, Vector2) {
 	let vect = target - first_point;
 	let dist = vect.length();
-	let min_len = (first_len - second_len).abs();
-	let max_len = first_len + second_len;
+	let min_length = (first_len - second_len).abs();
+	let max_length = first_len + second_len;
+	let len_diff = max_length - min_length;
+
+	let min_length_clamped = min_length + len_diff * min_target_distance_ratio.clamp(0.0, 1.0);
+	let max_length_clamped =
+		max_length - len_diff * (1.0 - max_target_distance_ratio.clamp(0.0, 1.0));
 
 	let vect_normalized = vect / dist;
-	let dist_clamped = dist.clamp(min_len, max_len);
+	let (vect_clamped, dist_clamped) = if dist < min_length_clamped {
+		(vect_normalized * min_length_clamped, min_length_clamped)
+	} else if dist > max_length_clamped {
+		(vect_normalized * max_length_clamped, max_length_clamped)
+	} else {
+		(vect, dist)
+	};
 
 	let first_len_squared = first_len.powi(2);
 	let ah_cathetus_length =
 		(first_len_squared - second_len.powi(2) + dist_clamped.powi(2)) / (2.0 * dist_clamped);
-	let height = (first_len_squared - ah_cathetus_length.powi(2)).sqrt();
+	let height = (first_len_squared - ah_cathetus_length.powi(2))
+		.abs()
+		.sqrt();
 	let h_point = first_point + vect_normalized * ah_cathetus_length;
-	h_point
-		+ Vector2::new(
-			height * vect_normalized.y * stretch_factor,
-			-height * vect_normalized.x * stretch_factor,
-		)
+	(
+		h_point
+			+ Vector2::new(
+				height * vect_normalized.y * stretch_factor,
+				-height * vect_normalized.x * stretch_factor,
+			),
+		vect_clamped,
+	)
 }
--- tests/test_two_bone.rs
+++ tests/test_two_bone.rs
@@ -6,7 +6,7 @@ use vect::prelude::*;
 mod test_calculate_point_between {
 	use super::*;
 
-	const E: f64 = 0.0000001;
+	const E: f64 = 0.000001;
 
 	#[test]
 	fn test_calculate_point_between() {
@@ -16,10 +16,12 @@ mod test_calculate_point_between {
 			16.6,
 			8.6,
 			1.0,
+			0.0,
+			1.0,
 		);
 
-		assert_relative_eq!(result.x, 15.04, epsilon = E);
-		assert_relative_eq!(result.y, -7.02555336, epsilon = E);
+		assert_relative_eq!(result.0.x, 15.04, epsilon = E);
+		assert_relative_eq!(result.0.y, -7.02555336, epsilon = E);
 	}
 
 	#[test]
@@ -30,10 +32,12 @@ mod test_calculate_point_between {
 			16.6,
 			8.6,
 			0.16,
+			0.0,
+			1.0,
 		);
 
-		assert_relative_eq!(result.x, 15.04, epsilon = E);
-		assert_relative_eq!(result.y, -1.12408854, epsilon = E);
+		assert_relative_eq!(result.0.x, 15.04, epsilon = E);
+		assert_relative_eq!(result.0.y, -1.12408854, epsilon = E);
 	}
 
 	#[test]
@@ -44,10 +48,12 @@ mod test_calculate_point_between {
 			16.6,
 			8.6,
 			-0.62,
+			0.0,
+			1.0,
 		);
 
-		assert_relative_eq!(result.x, 15.04, epsilon = E);
-		assert_relative_eq!(result.y, 4.35584308, epsilon = E);
+		assert_relative_eq!(result.0.x, 15.04, epsilon = E);
+		assert_relative_eq!(result.0.y, 4.35584308, epsilon = E);
 	}
 
 	#[test]
@@ -58,10 +64,12 @@ mod test_calculate_point_between {
 			16.6,
 			8.6,
 			0.0,
+			0.0,
+			1.0,
 		);
 
-		assert_relative_eq!(result.x, 15.04, epsilon = E);
-		assert_relative_eq!(result.y, 0.0, epsilon = E);
+		assert_relative_eq!(result.0.x, 15.04, epsilon = E);
+		assert_relative_eq!(result.0.y, 0.0, epsilon = E);
 	}
 
 	#[test]
@@ -72,37 +80,91 @@ mod test_calculate_point_between {
 			16.6,
 			8.6,
 			3.23,
-		);
-
-		assert_relative_eq!(result.x, 15.04, epsilon = E);
-		assert_relative_eq!(result.y, -22.6925373, epsilon = E);
-	}
-
-	#[test]
-	fn test_when_target_is_too_far() {
-		let result = calculate_point_between_stretched(
-			Vector2::new(27.0, 0.0),
-			Vector2::new(0.0, 0.0),
-			18.0,
-			8.0,
+			0.0,
 			1.0,
 		);
 
-		assert_relative_eq!(result.x, 18.0, epsilon = E);
-		assert_relative_eq!(result.y, 0.0, epsilon = E);
+		assert_relative_eq!(result.0.x, 15.04, epsilon = E);
+		assert_relative_eq!(result.0.y, -22.6925373, epsilon = E);
 	}
 
 	#[test]
 	fn test_when_target_is_too_close() {
 		let result = calculate_point_between_stretched(
-			Vector2::new(9.0, 0.0),
+			Vector2::new(7.0, 0.0),
 			Vector2::new(0.0, 0.0),
-			18.0,
-			8.0,
+			16.6,
+			8.6,
+			1.0,
+			0.0,
 			1.0,
 		);
 
-		assert_relative_eq!(result.x, 18.0, epsilon = E);
-		assert_relative_eq!(result.y, 0.0, epsilon = E);
+		assert_relative_eq!(result.0.x, 16.6, epsilon = E);
+		assert_relative_eq!(result.0.y, 0.0, epsilon = E);
+	}
+
+	#[test]
+	fn test_when_target_is_too_far() {
+		let result = calculate_point_between_stretched(
+			Vector2::new(26.0, 0.0),
+			Vector2::new(0.0, 0.0),
+			16.6,
+			8.6,
+			1.0,
+			0.0,
+			1.0,
+		);
+
+		assert_relative_eq!(result.0.x, 16.6, epsilon = E);
+		assert_relative_eq!(result.0.y, 0.0, epsilon = E);
+	}
+
+	#[test]
+	fn test_with_custom_clamp_range() {
+		let result = calculate_point_between_stretched(
+			Vector2::new(20.0, 0.0),
+			Vector2::new(0.0, 0.0),
+			16.6,
+			8.6,
+			1.0,
+			0.3,
+			0.9,
+		);
+
+		assert_relative_eq!(result.0.x, 15.04, epsilon = E);
+		assert_relative_eq!(result.0.y, -7.02555336, epsilon = E);
+	}
+
+	#[test]
+	fn test_when_target_is_too_close_with_custom_clamp_range() {
+		let result = calculate_point_between_stretched(
+			Vector2::new(9.0, 0.0),
+			Vector2::new(0.0, 0.0),
+			16.6,
+			8.6,
+			1.0,
+			0.4,
+			0.7,
+		);
+
+		assert_relative_eq!(result.0.x, 14.2141935483871, epsilon = E);
+		assert_relative_eq!(result.0.y, -8.574188111360202, epsilon = E);
+	}
+
+	#[test]
+	fn test_when_target_is_too_far_with_custom_clamp_range() {
+		let result = calculate_point_between_stretched(
+			Vector2::new(26.0, 0.0),
+			Vector2::new(0.0, 0.0),
+			16.6,
+			8.6,
+			1.0,
+			0.4,
+			0.7,
+		);
+
+		assert_relative_eq!(result.0.x, 15.049940119760482, epsilon = E);
+		assert_relative_eq!(result.0.y, -7.0042346042679, epsilon = E);
 	}
 }
